e028daedd827ab3e670ec2bb30db4f36a158ec9b
Lucas Mitchel <lucasmitchelcampos17@gmail.com>
2022-12-06 05:04:58 -0300

    shore: Integrate PWA

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +17,16 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue(), svgLoader()],
+  plugins: [
+    vue(),
+    svgLoader(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
