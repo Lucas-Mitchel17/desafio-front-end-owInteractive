deaecd65f323d6869d4482233d48bda6ef0d4ac6
Lucas Mitchel <lucasmitchelcampos17@gmail.com>
2022-12-05 21:52:06 -0300

    feat: Create CartView

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,3 +1,19 @@
-<script setup></script>
+<script setup>
+import { CartLayout } from '@Modules';
+</script>
 
-<template>view</template>
+<template>
+  <MainLayout>
+    <section class="cart-section">
+      <CartLayout />
+    </section>
+  </MainLayout>
+</template>
+
+<style lang="sass">
+.cart-section
+  display: grid
+  max-width: $desktop
+  margin: 90px auto 0
+  padding: 0 10px
+</style>
